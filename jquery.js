e8a139bc5c3007491c45b72a5af26d6a16d90e7e
pharumo <pharumo@pharumos-MacBook-Air.local>
2018-10-05 14:26:44 +0800

    Added jQuery

---
+++ jquery.js
@@ -0,0 +1,39 @@
+var users = '[{"id": "1", "first_name": "Truong", "last_name": "Tran", "sex": "1", "birthday": "1988-04-01", "phone": "090-1234-5678", "country": "vn", "note": ""  }]';
+
+
+response = $.parseJSON(users);
+
+$(function () {
+    $.each(response, function (i, item) {
+        if (item.sex == 1) {
+          item.sex = "Male";
+        }else if (item.sex == 0){
+          item.sex = "Female";
+        }else {
+          item.sex = "Others"
+        }
+      
+        if (item.country == "vn")  {
+          item.country = "Vietnam";
+        }else if (item.country == "ms") {
+          item.country = "Malaysia";
+        }else {
+          item.country = "Japan";
+        }
+      
+        $('<tr>').append(
+        $('<td>').text(item.id),
+        $('<td>').text(item.first_name + "" + item.last_name),
+        $('<td>').text(item.sex),
+        $('<td>').text(item.birthday),
+        $('<td>').text(item.phone),
+        $('<td>').text(item.country),
+        $('<td>').text(item.note)).appendTo('#records_table');
+    });
+  
+    $( "form" ).submit(function( event ) {
+      console.log( $( this ).serializeObject() );
+
+      //event.preventDefault();
+    });
+});
