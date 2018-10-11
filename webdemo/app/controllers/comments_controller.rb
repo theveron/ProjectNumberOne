809127a5326ff5cc9dedc3fe8c535dcc902e3932
pharumo <pharumo@pharumos-MacBook-Air.local>
2018-10-11 18:25:57 +0800

    added channel comments

--- webdemo/app/controllers/comments_controller.rb
+++ webdemo/app/controllers/comments_controller.rb
@@ -3,7 +3,11 @@ class CommentsController < ApplicationController
     
     def create
         # it looks up the post that we are trying to add comments to and creates that
-        @post.comments.create! comments_params
+            #@post.comments.create! comments_params
+        comment = @post.comments.create! comments_params #after we create a comment, lets save that to local variable
+        # call the comments mailer
+        # deliver_later means that we dont want to tie up the respond to this action and sending the email in live, we use this which will trigger the job framework for ruby on rails to send this email asynchronously, even the render of the of the email will happen asynchronously outside of the main request responds flow
+        CommentsMailer.submitted(comment).deliver_later
         # after that, it redirects back to @post, by redirecting to @post, we did the same lookup as we did the render, where it looks up for the routes for the post matches the model
         redirect_to @post
     end
